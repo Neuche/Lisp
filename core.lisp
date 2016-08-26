8eb00dec3b383d54eb9585e396b8ab2c9d6764f8
Neuche <igna.eg@gmail.com>
2016-08-26 11:43:14 -0300

    Primeros pasos..

---
+++ core.lisp
@@ -0,0 +1,5 @@
+(defparameter *nodes* '(sala (Estas en la sala. Es una cagada))
+						(pieza (Estas en la pieza. Es un quilombo))
+						(patio (Estas en el patio. Hay un perro bloqueando la puerta)))
+
+(print "Algo para chequear")
